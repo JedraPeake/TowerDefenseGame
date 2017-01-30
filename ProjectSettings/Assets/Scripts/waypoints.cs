58fc36c4a0feb753ea96d1e17a04b50c25b6d61c
JedraPeake <jpeake2@uwo.ca>
2017-01-29 19:47:49 -0500

    Enemy UI wave spawner added

---
+++ ProjectSettings/Assets/Scripts/waypoints.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class waypoints : MonoBehaviour {
+
+    public static Transform[] points;
+
+
+	// Use this for initialization
+	void Awake () {
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < points.Length; i++) {
+            points[i] = transform.GetChild(i);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
