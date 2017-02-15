07263d695fbdd598213458d581b6a319c504d495
JedraPeake <jpeake2@uwo.ca>
2017-02-14 23:59:11 -0500

    Turret Building done

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildManager : MonoBehaviour {
+
+    private GameObject turretToBuild;
+    public static BuildManager instance;
+    public GameObject standardTurretPrefab;
+
+    void Awake() {
+        if (instance != null) {
+            Debug.LogError("More then one build manager");
+        }
+        instance = this;
+
+    }
+
+    public GameObject GetTurretToBuild() {
+        return turretToBuild;
+    }
+
+	// Use this for initialization
+	void Start () {
+        turretToBuild = standardTurretPrefab;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+
+    public Color hoverColor;
+    private Renderer rend;
+    private Color startColor;
+    private GameObject turret;
+    public Vector3 positionOffset;
+
+    void OnMouseEnter() {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseDown() {
+        if (turret != null) {
+            Debug.Log("Can't build there- TODO DISPLAY ON SCREEN");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent < Renderer >();
+        startColor = rend.material.color;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
